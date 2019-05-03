c823ff1ab26a835a55d32e24338290fc5c7e3b0a
drohnwynandrt <dw.ramdattewari@gmail.com>
2019-05-03 10:39:50 +0200

    Added jQuery to change the background color for the navigation bar elements on hover

---
+++ frontpagedierenpark.js
@@ -0,0 +1,16 @@
+
+
+   $('.navhome').hover(function(){
+        $(this).toggleClass('navhome_hover');})
+
+
+/*   
+
+$('ul.navigationbar li:nth-child(3n)').css('background-color', 'rgb(171, 234, 236)'); 
+
+ $('ul.navigationbar li:nth-child(3n').hover(function(){
+        $(this).css('background-color', 'rgb(183, 203, 219)');})
+
+
+
+*/
